5c9e9620819e59adc5cac88c4958b8deb926e592
Mohanraj Sivanantham <mohanraj.shiva@gmail.com>
2024-08-19 05:50:52 +0530

    Menu display based on login implemented with old method

--- client/src/app/nav/nav.component.ts
+++ client/src/app/nav/nav.component.ts
@@ -1,11 +1,12 @@
 import { Component, inject } from '@angular/core';
 import { FormsModule } from '@angular/forms';
 import { AccountService } from '../services/account-service.service';
+import { NgIf } from '@angular/common';
 
 @Component({
   selector: 'app-nav',
   standalone: true,
-  imports: [FormsModule],
+  imports: [FormsModule,NgIf],
   templateUrl: './nav.component.html',
   styleUrl: './nav.component.css'
 })
@@ -18,9 +19,11 @@ export class NavComponent {
   {
       this.accountService.Login(this.model).subscribe({
         next:response => {
+          this.isLoggedIn=true;
           console.log(response);
         },
         error:error =>{
+          this.isLoggedIn=false;
           console.log(error);
         }
   });
