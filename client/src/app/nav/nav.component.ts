90dc75c0d9305721162a2ed626a0a8ab88c6fc19
Mohanraj Sivanantham <mohanraj.shiva@gmail.com>
2024-08-28 07:52:37 +0530

    routing - links configured

--- client/src/app/nav/nav.component.ts
+++ client/src/app/nav/nav.component.ts
@@ -2,18 +2,22 @@ import { Component, inject, OnInit } from '@angular/core';
 import { FormsModule } from '@angular/forms';
 import { AccountService } from '../services/account-service.service';
 import { BsDropdownModule } from 'ngx-bootstrap/dropdown';
+import { Router, RouterLink, RouterLinkActive } from '@angular/router';
 
 @Component({
   selector: 'app-nav',
   standalone: true,
   imports: [FormsModule,
-    BsDropdownModule
+    BsDropdownModule,
+    RouterLink,
+    RouterLinkActive
   ],
   templateUrl: './nav.component.html',
   styleUrl: './nav.component.css'
 })
 export class NavComponent  {
   accountService = inject(AccountService)
+  router = inject(Router)
   isLoggedIn:boolean = false;
   model:any = {};
 
@@ -22,8 +26,9 @@ export class NavComponent  {
   {
       this.accountService.Login(this.model).subscribe({
         next:response => {
-          this.isLoggedIn=true;
-          console.log(response);
+          // this.isLoggedIn=true;
+          // console.log(response);
+          this.router.navigateByUrl("/members");
         },
         error:error =>{
           this.isLoggedIn=false;
@@ -34,5 +39,6 @@ export class NavComponent  {
   logout()
   {
     this.accountService.logout();
+    this.router.navigateByUrl("/");
   }
 }
