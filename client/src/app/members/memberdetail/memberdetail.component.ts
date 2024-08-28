3c83e39ac5e69223b796d0e3d275e2982ada6fa3
Mohanraj Sivanantham <mohanraj.shiva@gmail.com>
2024-08-28 06:08:24 +0530

    routing -  components added

---
+++ client/src/app/members/memberdetail/memberdetail.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-memberdetail',
+  standalone: true,
+  imports: [],
+  templateUrl: './memberdetail.component.html',
+  styleUrl: './memberdetail.component.css'
+})
+export class MemberdetailComponent {
+
+}
