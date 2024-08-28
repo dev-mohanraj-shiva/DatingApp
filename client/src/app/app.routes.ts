ca73fdef399491537abdd6a7d8dd1b65d403bf62
Mohanraj Sivanantham <mohanraj.shiva@gmail.com>
2024-08-28 06:13:25 +0530

    routing - routes were created and configured

--- client/src/app/app.routes.ts
+++ client/src/app/app.routes.ts
@@ -1,3 +1,15 @@
 import { Routes } from '@angular/router';
+import { HomeComponent } from './home/home.component';
+import { MembersComponent } from './members/members/members.component';
+import { MemberdetailComponent } from './members/memberdetail/memberdetail.component';
+import { ListComponent } from './lists/list/list.component';
+import { MessagesComponent } from './messages/messages.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  {path:"",component:HomeComponent},
+  {path:"members",component:MembersComponent},
+  {path:"members/:id",component:MemberdetailComponent},
+  {path:"lists",component:ListComponent},
+  {path:"messages",component:MessagesComponent},
+  {path:"**",component:HomeComponent},
+];
